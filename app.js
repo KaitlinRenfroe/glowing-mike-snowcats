a82be0d7b068794937e05feca67b1b6cbf496751
Kaitlin Renfroe <kaywren017@gmail.com>
2024-01-25 19:16:59 -0600

    added port binding

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 4000
 
 app.get('/', function (req, res) {
   res.send('Hello World from Express')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Mike app listening on port ${port}`)
+})
